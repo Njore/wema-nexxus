6a09671f815721e24dd48a8f54c5431504336c5c
aligularoy <aligularoy@gmail.com>
2026-02-24 17:55:36 +0300

    Refactor: update components, remove old team pages, and update vite config

---
+++ src/constants/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='background-dark text-light p-4'>
+        <p>hello</p>
+
+    </div>
+  )
+}
+
+export default Header
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../constants/Header'
+import Footer from '../constants/Footer'
+
+const Home = () => {
+  return (
+    <div>
+        hello
+    </div>
+  )
+}
+
+export default Home
